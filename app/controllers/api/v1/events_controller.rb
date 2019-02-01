493526d6f0dddabc549881a3172b398b5fdbcc9d
Anderson Chen <ander.chen00@gmail.com>
2019-02-01 16:26:41 -0200

    Created the events#create but problems with logic

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -3,8 +3,30 @@ class Api::V1::EventsController < Api::V1::BaseController
     @events = Event.all
   end
 
+  def create
+    @event = Event.new(event_params)
+    @event.actor = Actor.first
+    @event.repo = Repo.first
+    if @event.save
+      render :index, status: :created
+    else
+      render_error
+    end
+  end
+
   def erase
     Event.destroy_all
     head '200'
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:type)
+  end
+
+  def render_error
+    render json: { errors: @event.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
