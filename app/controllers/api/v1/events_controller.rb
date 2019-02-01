cc1a23af0398522ced61f833ce6d8c26ccf112e4
Anderson Chen <ander.chen00@gmail.com>
2019-02-01 15:39:59 -0200

    Modified the route so that the DELETE would be events#erase, also made the correspondent methods for it"

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::EventsController < Api::V1::BaseController
-
   def index
     @events = Event.all
+  end
+
+  def erase
+    Event.destroy_all
+    head '200'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :events, only: [:index, :create]
+      delete 'erase', to: 'events#erase'
     end
   end
 
